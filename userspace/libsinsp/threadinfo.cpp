c9dd166e454cceb67f787c61e5db3df247298457
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2018-07-09 19:34:43 +0200

    Add log messages about handling large environments

--- userspace/libsinsp/threadinfo.cpp
+++ userspace/libsinsp/threadinfo.cpp
@@ -521,7 +521,12 @@ void sinsp_threadinfo::set_env(const char* env, size_t len)
 		// this may fail for short-lived processes
 		if (set_env_from_proc())
 		{
+#ifdef _DEBUG
+			g_logger.format(sinsp_logger::SEV_DEBUG, "Large environment for process %lu [%s], loaded from /proc", m_pid, m_comm.c_str());
+#endif
 			return;
+		} else {
+			g_logger.format(sinsp_logger::SEV_INFO, "Failed to load environment for process %lu [%s] from /proc, using first %d bytes", m_pid, m_comm.c_str(), SCAP_MAX_ENV_SIZE);
 		}
 	}
 
